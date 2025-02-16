83e9ea169a53424b56f7c3e0e017766780a40d58
nikogalvan <ramiro_coronel333@hotmail.com>
2025-02-15 23:02:14 -0300

    feat(Service): add method to create product

--- src/main/java/com/rami/ecommerce/Product/ProductService.java
+++ src/main/java/com/rami/ecommerce/Product/ProductService.java
@@ -1,26 +1,36 @@
 package com.rami.ecommerce.Product;
 
+import java.sql.Timestamp;
 import java.util.List;
-import java.util.Optional;
-
 import org.springframework.stereotype.Service;
+import lombok.RequiredArgsConstructor;
 
+//VALIDAR SERVICE
 @Service
+@RequiredArgsConstructor
 public class ProductService {
 
   private final ProductRepository productRepository;
 
-  public ProductService(ProductRepository productRepository) {
-    this.productRepository = productRepository;
+  public List<ProductDTO> getAllProducts() {
+    return productRepository.findAll()
+        .stream()
+        .map(
+            product -> new ProductDTO(product.getId(), product.getName(), product.getDescription(), product.getPrice()))
+        .toList();
   }
 
-  public Product getProductById(Long id) {
-    Optional<Product> productFinded = productRepository.findById(id);
-    return productFinded.isPresent() ? productFinded.get() : null;
-  }
+  public ProductDTO createProduct(ProductDTO dtoProduct) {
+    Product product = new Product();
+    product.setName(dtoProduct.name());
+    product.setDescription(dtoProduct.description());
+    product.setPrice(dtoProduct.price());
+    product.setCreatedAt(new Timestamp(System.currentTimeMillis()));
+    product.setUpdatedAt(new Timestamp(System.currentTimeMillis()));
 
-  public List<Product> getProducts() {
-    return productRepository.findAll();
+    Product savedProduct = productRepository.save(product);
+    return new ProductDTO(savedProduct.getId(), savedProduct.getName(), savedProduct.getDescription(),
+        savedProduct.getPrice());
   }
 
 }
